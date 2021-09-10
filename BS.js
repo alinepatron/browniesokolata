92296d106c7852b70240e69a83404d2081d64d1c
alinepatron <0214161@up.edu.mx>
2021-09-10 16:36:10 -0500

    2

--- BS.js
+++ BS.js
@@ -1,18 +1,13 @@
 function email() {
-    const e = document.getElementById("1").value;
-    const a = document.getElementById("2").value;
-    const m = document.getElementById("3").value;
-    alert(e+a+m);
+    const a = document.getElementById("1").value;
+    const m = document.getElementById("2").value;
+    const b = 'mailto:0214161@up.edu.mx?subject='+a+'&body='+m;
 
-    Email.send({
-        Host: "smtp.gmail.com",
-        To : '0214161@up.edu.mx',
-        From : e,
-        Subject : a,
-        Body : m,
-        });
+    document.getElementById("3").href=b;
+
 }
 
 function alerta() {
-    alert ('Tu mensaje ha sido enviado satisfactoriamente');
+    alert ('Procesando su mensaje... Por favor confirme el pedido');
 }
+
